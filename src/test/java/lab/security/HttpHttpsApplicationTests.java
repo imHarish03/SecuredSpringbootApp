536b5e113379a2b65428143c1444fa9b1072fc7f
imHarish03 <harishhari302@gmail.com>
2020-12-16 18:37:13 +0530

    initial commit

---
+++ src/test/java/lab/security/HttpHttpsApplicationTests.java
@@ -0,0 +1,13 @@
+package lab.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpHttpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
